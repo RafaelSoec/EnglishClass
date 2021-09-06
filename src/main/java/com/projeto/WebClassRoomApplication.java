d6f51ba6a6b300446201e97cf45620a11a89be1e
Rafael Souza <rafaelsoec@gmail.com>
2021-09-06 19:20:36 -0300

    build versao inicial

--- src/main/java/com/projeto/WebClassRoomApplication.java
+++ src/main/java/com/projeto/WebClassRoomApplication.java
@@ -3,7 +3,6 @@ package com.projeto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
